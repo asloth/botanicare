fec9b03aa32c83bf670c04d1b1ed13eaedbd95a1
Sara <41219424+asloth@users.noreply.github.com>
2020-11-14 21:12:45 -0500

    menubar arreglado

--- lib/src/ui/MenuBar.dart
+++ lib/src/ui/MenuBar.dart
@@ -4,6 +4,7 @@ import 'package:botanicare/src/ui/CalculatorScreen.dart';
 import 'package:botanicare/src/ui/PerfilScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MenuBar extends StatefulWidget {
   const MenuBar({Key key}) : super(key: key);
@@ -17,14 +18,15 @@ class _MenuBarState extends State<MenuBar> {
   GlobalKey _bottomNavigationKey = GlobalKey();
   final pages = [
     HomeScreen(),
-    CalculatorScreen(),
     Text('hola soy garden screen'),
-    Text('hola soy alarmas'),
+    CalculatorScreen(),
     PerfilScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: pages[_page],
       bottomNavigationBar: CurvedNavigationBar(
@@ -32,29 +34,25 @@ class _MenuBarState extends State<MenuBar> {
         index: 0,
         height: 50.0,
         items: <Widget>[
-          Icon(
-            Icons.home,
-            size: 30,
-            color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SvgPicture.asset('assets/icons/home_outline.svg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SvgPicture.asset(
+              'assets/icons/leaf.svg',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SvgPicture.asset(
+              'assets/icons/bitacora.svg',
+            ),
           ),
           Icon(
-            Icons.attach_money,
-            size: 30,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.local_florist,
-            size: 30,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.notifications,
-            size: 30,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.person,
-            size: 30,
+            Icons.person_outline,
+            size: 35,
             color: Colors.white,
           ),
         ],
