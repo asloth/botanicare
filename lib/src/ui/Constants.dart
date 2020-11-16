031d0bddd9e7de40abd6c1414f21314e724fe360
Sara <41219424+asloth@users.noreply.github.com>
2020-11-15 23:26:19 -0500

    se agrego color para icono modificar perfil

--- lib/src/ui/Constants.dart
+++ lib/src/ui/Constants.dart
@@ -5,6 +5,7 @@ const kPrimaryColor = Color(0xFF3ED400);
 const kTextColor = Color(0xFF1D3708);
 const kBackgroundColor = Color(0xFFEEEBEB);
 const kBackgroundColor2 = Color(0xFFFFFFFF);
+const kModifyIconColor = Color(0xFF291C84);
 
 //padding
 const double kDefaultPadding = 20.0;
